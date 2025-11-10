3fb9c2628fbb85505d42c7926f1ecaa2c85b438a
Issac Liu <issac@matchatea.dev>
2025-11-10 03:04:24 -0500

    feat: update HomePage to use dynamic logo source and simplify layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
 import { ArrowDown } from "lucide-react";
+import { get } from "http";
+import { getLogoSrc } from "~/components/get-logo";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-[#032d64] to-[#15162c] text-white">
-      <div className="relative container flex h-screen items-center justify-center px-4 py-16">
-        <div className="relative flex w-full max-w-[min(92vw,1200px)] flex-col items-center justify-center sm:flex-row">
+      <div className="container flex h-screen items-center justify-center px-4 py-16">
+        <div className="flex w-full max-w-[min(92vw,1200px)] flex-col items-center justify-center">
           <Image
-            src="/intodeep-vector.svg"
+            src={getLogoSrc("into-the-deep", "tagline")}
             alt="Logo"
-            width={600}
-            height={500}
-            className="z-0 mr-[30%] -mb-[30%] -ml-[5%] h-auto w-[min(65vw,700px)] pl-[0%] sm:-mr-[10%] sm:mb-[10%] sm:ml-[10%] sm:pl-[10%]"
+            width={800}
+            height={700}
+            className="h-auto w-[min(80vw,1000px)]"
           />
-          <div className="z-12 mt-[0%] -mr-[40%] flex flex-col text-right leading-none text-yellow-400 text-shadow-black text-shadow-lg sm:mt-[8%] sm:mr-[20%] md:mr-[15%] md:text-left">
-            <h2 className="mx-[1%] text-[clamp(1rem,4.2vw,2rem)]/[120%] font-light tracking-wider">
-              WE ARE THE
-            </h2>
-            <h1 className="font-regular text-[clamp(3.5rem,9vw,8rem)]/[80%] tracking-tighter">
-              Byte
-              <br />
-              Knights
-            </h1>
-            <h2 className="mx-[1%] text-[clamp(1.25rem,4.2vw,3.6rem)]/[90%] font-light">
-              FTC TEAM 27705
-            </h2>
-          </div>
         </div>
         <ArrowDown
           className="absolute bottom-8 left-1/2 z-50 -translate-x-1/2 animate-bounce text-yellow-400"
