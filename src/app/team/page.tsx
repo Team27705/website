a796202f72999ac132adfb410b3161b8524354e2
Issac Liu <issac@matchatea.dev>
2025-12-11 21:38:43 -0500

    fix: wrong id meant hero doesnt affect header scroll

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -14,7 +14,7 @@ export default function TeamPage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-[#032d64] to-[#15162c] text-white">
       <div
         className="relative container flex h-[35vh] max-h-[35vh] items-center justify-center px-4 pt-32"
-        id="team-hero"
+        id="hero"
       >
         <div className="flex w-full max-w-[min(92vw,1200px)] flex-col items-center justify-center">
           <h1 className="text-6xl font-bold text-yellow-400">Meet Our Team</h1>
