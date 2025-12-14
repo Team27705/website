c9fa4bc0ee018969cf723f01f455bcf2801e701f
Issac Liu <issac@matchatea.dev>
2025-12-14 17:52:18 -0500

    feat: add description of the ByteKnights team and update homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const videoOpacityLevel = 0.3;
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-[#032d64] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-[#032d64] to-[#15162c] text-white mb-30">
       <div
         className="relative flex h-screen max-h-screen w-full items-center justify-center"
         id="hero"
@@ -50,13 +50,14 @@ export default function HomePage() {
         </h1>
         <TeamEventsComponent />
       </section>
-      {/* Header is rendered in the layout to keep it fixed to the viewport */}
-      {
-        /* Stuff to make this scroll */
-        Array.from({ length: 10 }).map((_, i) => (
-          <div key={i} className="my-8 h-32 w-full text-center" >holy guacamole what a cool homepage x{i+1}</div>
-        ))
-      }
+      <section className="mt-15">
+        <h1 className="my-20 text-center text-4xl font-bold text-white md:text-5xl lg:text-6xl">
+          OUR STORY
+        </h1>
+        <p className="mx-4 max-w-3xl text-center text-lg leading-relaxed text-white sm:mx-0 sm:text-xl">
+          In 2024, the FTC Brooklyn ByteKnights was formed. As a rookie, we used a kit bot and with the limited resources and knowledge we had, we did the best we could. But for our second year, we’re ready to kick it off with a robot of our own! Get ready for this year’s season of FTC as the ByteKnights are ready to crush it!
+        </p>
+      </section>
     </main>
   );
 }
